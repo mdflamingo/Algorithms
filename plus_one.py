666a94affe3d71f112dfb4a94a54cc2f52beabfb
Анастасия Искоскина <aiskoskina@yandex.ru>
2023-12-06 16:32:03 +0300

    change pluse_one.py

--- plus_one.py
+++ plus_one.py
@@ -4,9 +4,9 @@ from typing import List
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         tmp = [str(el) for el in digits]
-        tmp_digit = ''.join(tmp)
-        return tmp_digit
+        tmp_digit = str(int(''.join(tmp)) + 1)
+        return [int(el) for el in tmp_digit]
 
 
 a = Solution()
-print(a.plusOne([1, 2, 3]))
+print(a.plusOne([9]))

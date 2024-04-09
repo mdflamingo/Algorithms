6c16dc4bd697663fdc4cf6a024f28cba66d2431b
Анастасия Искоскина <aiskoskina@yandex.ru>
2024-04-09 11:56:48 +0300

    add new algorithms

--- prefix.py
+++ prefix.py
@@ -15,9 +15,7 @@ for _ in range(q):
 
 sort_worker = sorted(worker)
 for el in requests:
-    el_in_sort = [name for name in sort_worker if name.startswith(el[1])]
+    el_in_sort = [name for index, name in enumerate(sort_worker) if name.startswith(el[1])]
     print(el_in_sort)
-    res = [el[index] for index, el in enumerate(el_in_sort) if len(el_in_sort) > 1]
-    print(res)
-    # el_in_worker = [index for index, name in enumerate(worker) if name.startswith(el_in_sort[el[0]])]
-    # print(el_in_worker)
+    el_in_worker = [index + 1 for index, name in enumerate(worker) if name == el_in_sort[el[0] - 1]]
+    print(el_in_worker)

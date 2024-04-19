279c64288ebbffadaa9b372d594fb1b5a9d17762
Анастасия Искоскина <aiskoskina@yandex.ru>
2024-04-19 13:24:13 +0300

    add new algorithms

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -1,24 +1,24 @@
 from typing import List
-import math
+
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left = [1]
-        right = [1]
 
-        for index, element in enumerate(nums[1:]):
-            t = nums[:index]
-            print(t)
-            r = math.prod(nums[index:])
-            left.append(r)
+        ans_right = [1]
+        ans_left = [1]
 
-        for index, element in enumerate(nums[::1]):
-            right.append(nums[index - 1])
+        for el in range(1, len(nums)):
+            ans_right.append(ans_right[-1] * nums[el - 1])
 
-        # print(left)
-        # print(right)
-        return left
+        for el in range(len(nums)-1, -1, -1):
+            t = ans_left[-1] * nums[el]
+            ans_left.append(t)
+
+        result = [x * y for x, y in zip(ans_right, list(reversed(ans_left[:len(nums)])))]
+
+        return result
 
 
 a = Solution()
-print(a.productExceptSelf([1, 2, 3, 4]))
+print(a.productExceptSelf([-1,1,0,-3,3]))
 
